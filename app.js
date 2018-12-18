c34bc630c82a0693353141522c76fd3c706aef93
Brian Burkard <brianburkard@gmail.com>
2018-12-18 18:38:50 -0500

    changed model to array

--- app.js
+++ app.js
@@ -9,31 +9,32 @@
 /* ====== MODEL ===== */
 
   const model = {
-     Henry: {
+    cats: [
+    {
       name: 'Henry',
       url: 'https://www.demilked.com/magazine/wp-content/uploads/2016/08/biggest-maine-coon-cat-photography-robert-sijka-thumb640.jpg',
       counter: 0
     },
-     Zuzu: {
+    {
       name: 'Zuzu',
       url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRet5cHD_iUrmo3TSWo7rSlJgWbMGpRqtrCVhe281MgHRRx443MaA',
       counter: 0
     },
-     Tortie: {
+    {
       name: 'Tortie',
       url: 'https://img.thrfun.com/img/024/498/tortoiseshell_cats_l1.jpg',
       counter: 0
     },
-     Chubs: {
+    {
       name: 'Chubs',
       url: 'https://i2-prod.mirror.co.uk/incoming/article11515078.ece/ALTERNATES/s615/1_plus-sized-cutie.jpg',
       counter: 0
     },
-     Tiger: {
+    {
       name: 'Tiger',
       url: 'https://s.yimg.com/uu/api/res/1.2/9fi1nf4iWS6bfdyCyXmbBg--~B/aD0zMzk7dz01MDk7c209MTthcHBpZD15dGFjaHlvbg--/http://media.zenfs.com/en-GB/homerun/newsweek_europe_news_328/296a853a5f736de51ee472ca1543153c',
       counter: 0
-    },
+    }],
   }
   
 
@@ -42,37 +43,54 @@
   const octopus = {
     
     getCats: function(){
-      return Object.values(model);
+      return model.cats;
     },
     
     showCat: function(cat){
       //console.log(e)
+      Object.assign(octopus.active, cat); // Assigns current cat to Octopus.active
       catView.render(cat)
       adminView.render();
     },
     
     init: function(){
       catView.init();
-      catView.render(Object.entries(model)[0][1]); // Renders first object in the model.
+      catView.render(model.cats[0]); // Renders first object in the model.
       listView.init();
       adminView.render();
+      adminView.init();
     },
     
     incrementCounter: function(cat){ // When image is clicked 
-      model[cat].counter++;
-      octopus.showCat(model[cat]); 
+      console.log(cat);
+      octopus.active.counter++;
+      octopus.showCat(octopus.active);
+      octopus.active.update(cat);
       /* Calls the showCat function that renders everything.
          Could be modified to only update the counter by writing new
          view functions that seperate these concerns.
       */
     },
 
-    
     active: {
       name: '',
       url: '',
-      counter: 0
-  },
+      counter: 0,
+      
+      update: function(cat){ // Updates the model.
+        model.cats[cat].name = this.name;
+        model.cats[cat].counter = this.counter;
+        model.cats[cat].url = this.url;
+      },
+
+      adminUpdate: function(){
+        let newName = document.querySelector('.name').value;
+        model.cats[this.name].name = newName;
+        model.cats[this.name].alt = newName;
+        octopus.showCat(model.cats[this.name]);
+        listView.init();
+      }
+    },
 
   }
 
@@ -82,10 +100,13 @@
   const catView = {
     
     init: function(){ 
-        catImage.addEventListener('click', function(e){ // Event liustener for when image is clicked.
-            let targetObj = e.target.alt;  // If it works it works.
-            octopus.incrementCounter(targetObj);
-        });
+        catImage.addEventListener('click', (function(e){ // Event liustener for when image is clicked.
+            return function(e){
+              let Obj = e.target.alt;  // If it works it works.
+              // console.log(e.target);
+            octopus.incrementCounter(Obj);
+          };
+        })());
     },
     
     render: function(cat){
@@ -93,12 +114,12 @@
       catArea.children[1].src = cat.url; // Fragile way to select elements.
       catArea.children[1].alt = cat.name; // Important for my image clicking event listener.
       catArea.children[2].innerHTML = `Counter: ${cat.counter}`;
-      Object.assign(octopus.active, cat); // Assigns current cat to Octopus.active
     }
   }
   
   const listView = {
     init: function(){
+      navDiv.innerHTML = '<ul></ul>';
        octopus.getCats().forEach(function(e){
          let catLi = document.createElement('li');
          catLi.innerHTML = `<h2>${e.name}</h2>`;
@@ -107,10 +128,27 @@
          });
          navDiv.firstElementChild.appendChild(catLi);
        }
-      )}
+      )},
+
+      update: function(){
+        let catLi = document.querySelectorAll('li');
+        octopus.getCats().forEach(function(e){
+          catLi.innerHTML = `<h2>${e.name}</h2>`;
+        });
+      }
   }
 
   const adminView = {
+
+    init: function(){
+      adminMenu.save.addEventListener('click', (function(e){
+        return function(e){
+          e.preventDefault();
+          octopus.active.adminUpdate();
+        }
+      })());
+    },
+
     render: function(){
       adminMenu.name.value = octopus.active.name;
       adminMenu.url.value = octopus.active.url;
